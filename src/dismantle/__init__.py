4590a54922827640de408fe1d8eba245e88dc108
Gary Stidston-Broadbent <openpgp@garysb.com>
2021-06-02 08:12:15 +0100

    docs: add ascii art because I is kewl

--- src/dismantle/__init__.py
+++ src/dismantle/__init__.py
@@ -1,3 +1,8 @@
+#      888 ,e,                                     d8   888
+#  e88 888  "   dP"Y 888 888 8e   ,"Y88b 888 8e   d88   888  ,e e,
+# d888 888 888 C88b  888 888 88b "8" 888 888 88b d88888 888 d88 88b
+# Y888 888 888  Y88D 888 888 888 ,ee 888 888 888  888   888 888   ,
+#  "88 888 888 d,dP  888 888 888 "88 888 888 888  888   888  "YeeP"
 # Copyright 2021 Gary Stidston-Broadbent
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +16,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+# =============================================================================
 """Dismantle is a Python package / plugin / extension manager."""
 from dismantle.__version__ import __version__  # noqa: F401
