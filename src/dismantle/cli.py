8ee981c210ea5e6227fd48dc99758b51adec119b
Gary Stidston-Broadbent <openpgp@garysb.com>
2021-06-02 08:25:57 +0100

    feat: add initial cli with version and help

--- src/dismantle/cli.py
+++ src/dismantle/cli.py
@@ -12,3 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+"""Check packages."""
+import argparse
+import sys
+from . import __version__
+
+
+def main(args=None):
+    """Main command line entry point."""
+    args = args if args else sys.argv[1:]
+    parser = argparse.ArgumentParser(
+        'dismantle',
+        description='Dismantle package management'
+    )
+    app = '%(prog)s version ' + __version__
+    parser.add_argument('-v', '--version', action='version', version=app)
+    args = parser.parse_args(args)
+    # args.func(args)
