98b7f95497d2c062a5358d409581e5707e5d202a
Gary Stidston-Broadbent <openpgp@garysb.com>
2021-06-02 08:13:12 +0100

    refactor: clean up unneeded docstring

--- src/dismantle/__version__.py
+++ src/dismantle/__version__.py
@@ -11,6 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
-"""Provide a central place to update the version number."""
+# =============================================================================
 __version__ = '0.1.0'
