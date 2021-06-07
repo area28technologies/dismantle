7ddce75d43f1071339d05f35cbeafcc4d750d448
Gary Stidston-Broadbent <openpgp@garysb.com>
2021-06-07 18:06:59 +0100

    refactor: add proper typing for extensions

--- src/dismantle/extension/iextension.py
+++ src/dismantle/extension/iextension.py
@@ -15,26 +15,28 @@
 class IExtension:
     """Extension interface to recognise what needs to be processed."""
 
-    _name = "default"
-    _category = "default"
+    _name: str = "default"
+    _category: str = "default"
 
-    def __init__(self):
-        self._active = False
+    def __init__(self) -> None:
+        self._active: bool = False
 
-    def activeate(self):
+    def activeate(self) -> bool:
         self._active = True
+        return self._active
 
-    def deactivate(self):
+    def deactivate(self) -> bool:
         self._active = False
+        return not self._active
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
     @property
-    def category(self):
+    def category(self) -> str:
         return self._category
 
     @property
-    def active(self):
+    def active(self) -> bool:
         return self._active
