ebb16e733ea185f2f6b094e70dbcd39d073ba2fd
Gary Stidston-Broadbent <openpgp@garysb.com>
2022-05-20 18:49:43 +0200

    style: clean imports in init

--- src/dismantle/__init__.py
+++ src/dismantle/__init__.py
@@ -1,6 +1,6 @@
 """Dismantle is a Python package / plugin / extension manager."""
 import logging
-from dismantle.__version__ import __version__  # noqa: F401
 
+from dismantle.__version__ import __version__  # noqa: F401
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
