52866c79c3bbed49940aa074f93a1058f5d7a2e4
Gary Stidston-Broadbent <openpgp@garysb.com>
2022-05-20 18:48:15 +0200

    feat: use build_parser for document generation

--- src/dismantle/cli.py
+++ src/dismantle/cli.py
@@ -1,16 +1,23 @@
 """Check packages."""
 import argparse
 import sys
-from . import __version__
+
+from dismantle import __version__
+
+
+def build_parser():
+    """Build the argument for the cli."""
+    parser = argparse.ArgumentParser(
+        'dismantle',
+        description='Dismantle package management'
+    )
+    return parser
 
 
 def main(args=None):
     """Main command line entry point."""
     args = args if args else sys.argv[1:]
-    parser = argparse.ArgumentParser(
-        'dismantle',
-        description='Dismantle package management'
-    )
+    parser = build_parser()
     app = '%(prog)s version ' + __version__
     parser.add_argument('-v', '--version', action='version', version=app)
     args = parser.parse_args(args)
