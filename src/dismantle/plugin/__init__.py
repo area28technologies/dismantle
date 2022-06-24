cb270b77f00a4ddb82599da0709f34ce1ce0b95f
Gary Stidston-Broadbent <openpgp@garysb.com>
2022-06-24 17:35:17 +0200

    chore: removes some unneeded debug messages

--- src/dismantle/plugin/__init__.py
+++ src/dismantle/plugin/__init__.py
@@ -1,17 +1,17 @@
 """Plugin management."""
-from .iplugin import IPlugin
-from .plugin import plugin
-from .plugins import Plugins
-from .register import register
-
 _plugins = {}
 
+from dismantle.plugin.iplugin import IPlugin  # noqa: E402
+from dismantle.plugin.plugin import plugin  # noqa: E402
+from dismantle.plugin.plugins import Plugins  # noqa: E402
+from dismantle.plugin.register import register  # noqa: E402
+
 __all__ = [
+    '_plugins',
     'IPlugin',
     'register',
     'plugin',
-    'Plugins',
-    '_plugins'
+    'Plugins'
 ]
 
 # IDEAS: --- plugins ----------------------------
