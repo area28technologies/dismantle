6d87e2c9fb0445d4fd5e81225dde2a0a62b8d841
David Fundingsland <david@fundings.land>
2022-08-26 16:19:59 +0800

    refactor(cli): minor refactor to simplify and tidy up code

--- src/dismantle/cli.py
+++ src/dismantle/cli.py
@@ -7,17 +7,16 @@ from dismantle import __version__
 
 def build_parser():
     """Build the argument for the cli."""
-    parser = argparse.ArgumentParser(
+    return argparse.ArgumentParser(
         'dismantle',
         description='Dismantle package management'
     )
-    return parser
 
 
 def main(args=None):
     """Add entry point for python -m option."""
-    args = args if args else sys.argv[1:]
+    args = args or sys.argv[1:]
     parser = build_parser()
-    app = '%(prog)s version ' + __version__
+    app = f'%(prog)s version {__version__}'
     parser.add_argument('-v', '--version', action='version', version=app)
     args = parser.parse_args(args)
